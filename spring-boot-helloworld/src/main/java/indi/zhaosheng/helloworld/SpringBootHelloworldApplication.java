1a3195c61eceb469dc8027b0a8cacb5f1c337426
zhaosheng <muluo0217@163.com>
2020-05-16 10:10:12 +0800

    spring boot开始

---
+++ spring-boot-helloworld/src/main/java/indi/zhaosheng/helloworld/SpringBootHelloworldApplication.java
@@ -0,0 +1,22 @@
+package indi.zhaosheng.helloworld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+@SpringBootApplication
+public class SpringBootHelloworldApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootHelloworldApplication.class, args);
+    }
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "hello world!";
+    }
+}
