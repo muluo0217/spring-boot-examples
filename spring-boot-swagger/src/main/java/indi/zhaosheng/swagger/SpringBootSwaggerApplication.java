909e6d6f32999e14ea06a0ed40e43c1ba8eda759
zhaosheng <muluo0217@163.com>
2020-05-17 22:25:31 +0800

    去掉无用代码

--- spring-boot-swagger/src/main/java/indi/zhaosheng/swagger/SpringBootSwaggerApplication.java
+++ spring-boot-swagger/src/main/java/indi/zhaosheng/swagger/SpringBootSwaggerApplication.java
@@ -8,8 +8,6 @@ import org.springframework.context.ApplicationContext;
 public class SpringBootSwaggerApplication {
 
     public static void main(String[] args) {
-        ApplicationContext applicationContext;
-        applicationContext.register
         SpringApplication.run(SpringBootSwaggerApplication.class, args);
     }
 
